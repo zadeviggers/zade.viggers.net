8c8f1608c009ea9a5bfeee49f1b6377ef49fed0f
zadeviggers <zade.viggers@gmail.com>
2022-02-02 13:03:07 +1300

    fix wordle solver
    Signed-off-by: zadeviggers <zade.viggers@gmail.com>

--- src/tools/wordle-solver.tsx
+++ src/tools/wordle-solver.tsx
@@ -8,7 +8,7 @@ import {
 } from "solid-js";
 
 type Letter = string;
-
+const threshold = 5;
 export default function WordleSolver() {
 	const [greyLetters, setGreyLetters] = createSignal<Letter[]>([]);
 	const [yellowLetters, setYellowLetters] = createSignal<Letter[]>([]);
@@ -19,35 +19,69 @@ export default function WordleSolver() {
 		const text = await data.text();
 		return text.split(",");
 	});
+	const totalLength = () =>
+		greyLetters().length + yellowLetters().length + greenLetters().length;
 
 	createEffect(() => {
-		if (
-			greyLetters().length + yellowLetters().length + greenLetters().length >=
-			3
-		) {
-			const greenFiltered = words().filter((word) => {
-				if (
-					greenLetters().filter((letter, i) =>
-						letter === "?" ? true : word[i] === letter,
-					).length === greenLetters().length
-				)
-					return true;
-				return false;
-			});
-			const yellowFiltered = greenFiltered.filter((word) => {
-				const lettersToFilter = [...yellowLetters(), ...greenLetters()];
-				if (
-					lettersToFilter.filter((letter) => word.includes(letter)).length ===
-					lettersToFilter.length
-				)
-					return true;
-				return false;
-			});
-			const greyFiltered = yellowFiltered.filter((word) => {
-				if (greyLetters().find((letter) => word.includes(letter))) return false;
-				return true;
-			});
-			setPossibleWords(greyFiltered);
+		if (totalLength() >= 5) {
+			const requiredLetters = [
+				...yellowLetters(),
+				...greenLetters().filter((l) => l !== "?"),
+			];
+			console.log(requiredLetters);
+			const greyFiltered =
+				greyLetters().length === 0
+					? words()
+					: words().filter((word) => {
+							if (
+								greyLetters()
+									.filter((letter) => !requiredLetters.includes(letter))
+									.find((letter) => {
+										if (word.includes(letter)) {
+											return true;
+										} else {
+											return false;
+										}
+									})
+							) {
+								return false;
+							} else {
+								return true;
+							}
+					  });
+			const yellowFiltered =
+				yellowLetters().length === 0
+					? greyFiltered
+					: greyFiltered.filter((word) => {
+							if (
+								requiredLetters.filter((letter) => word.includes(letter))
+									.length === requiredLetters.length
+							) {
+								return true;
+							} else {
+								return false;
+							}
+					  });
+			const greenFiltered =
+				greenLetters().length === 0
+					? yellowFiltered
+					: yellowFiltered.filter((word) => {
+							if (
+								greenLetters().filter((letter, i) => {
+									if (letter === "?") {
+										return true;
+									} else if (word[i] === letter) {
+										return true;
+									} else {
+										return false;
+									}
+								}).length === greenLetters().length
+							)
+								return true;
+							return false;
+					  });
+
+			setPossibleWords(greenFiltered);
 		}
 	});
 
@@ -62,7 +96,7 @@ export default function WordleSolver() {
 			</Match>
 			<Match when={!words.loading && !words.error}>
 				<div class="stack">
-					<p>Enter at least 3 total letters to see suggestions.</p>
+					<p>Enter at least {threshold} total letters to see suggestions.</p>
 					<label for="greyed-out-letters">
 						<input
 							id="greyed-out-letters"
@@ -81,16 +115,11 @@ export default function WordleSolver() {
 						<input
 							id="yellow-letters"
 							type="text"
-							regexp="^[a-zA-Z]{0,5}$"
-							maxlength={5}
+							regexp="^[a-zA-Z]$"
 							value={yellowLetters().join("")}
 							onInput={(e) =>
 								setYellowLetters(
-									(e.target.value as string)
-										.trim()
-										.toUpperCase()
-										.split("")
-										.slice(0, 5),
+									(e.target.value as string).trim().toUpperCase().split(""),
 								)
 							}
 						/>{" "}
@@ -122,7 +151,17 @@ export default function WordleSolver() {
 						{possibleWords().length > 0 && ` (${possibleWords().length})`}:
 					</h2>
 					<ul>
-						<For each={possibleWords()} fallback={<li>No words found :(</li>}>
+						<For
+							each={possibleWords()}
+							fallback={
+								totalLength() < threshold ? (
+									<li>
+										Enter at least {threshold} characters to see suggestions.
+									</li>
+								) : (
+									<li>No words found :(</li>
+								)
+							}>
 							{(word, index) => <li data-index={index()}>{word}</li>}
 						</For>
 					</ul>
