bb7d84ee435c48fc2956195b4e2990935c8bf02f
Zade Viggers <zade@viggers.net>
2023-01-16 13:27:02 +1300

    Add abortsignals

--- src/components/new-wordle-solver/worker.ts
+++ src/components/new-wordle-solver/worker.ts
@@ -3,22 +3,63 @@ import type { Colour, FilterData } from "./shared";
 let wordList: string[] = [];
 let filterData: FilterData = {};
 
+let filterController: AbortController | undefined;
+
 self.addEventListener("message", (e) => {
-	if (e.data.msg === "filter-info" && !!e.data.data) {
-		filterData = e.data.data;
-		startFiltering();
+	switch (e.data.msg) {
+		case "filter-info": {
+			if (!e.data.data) return;
+			filterController?.abort("New filtering data arrived.");
+			filterData = e.data.data;
+			filterController = new AbortController();
+			startFiltering(filterController.signal);
+			break;
+		}
+		case "reload-words": {
+			loadWordlist();
+			break;
+		}
 	}
 });
 
-function startFiltering() {
+function startFiltering(signal: AbortSignal) {
+	if (wordList.length === 0)
+		return self.postMessage({
+			msg: "still-loading-wordlist",
+		});
+
+	let possibleWords: string[] = [];
+
+	for (const word of wordList) {
+		// Allow aborting during the filtering process
+		if (signal.aborted) return;
+	}
+
+	if (signal.aborted) return;
 	self.postMessage({
 		msg: "filtered-words",
-		data: wordList,
+		data: possibleWords,
 	});
 }
 
-const res = await fetch("/five-letter-words.txt");
-const data = await res.text();
-wordList = data.split("\n");
+async function loadWordlist() {
+	try {
+		self.postMessage({
+			msg: "started-loading-wordlist",
+		});
+		const res = await fetch("/five-letter-words.txt");
+		const data = await res.text();
+		wordList = data.split("\n");
+		self.postMessage({
+			msg: "loaded-wordlist",
+		});
+	} catch (err) {
+		self.postMessage({
+			msg: "failed-loading-wordlist",
+		});
+	}
+}
+
+loadWordlist();
 
 export {};
